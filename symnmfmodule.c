4ae14bf7598bc65ecc71897e65e467bfb14a5d28
MuhammadFanni <mohamad.abd.af@gmail.com>
2026-07-01 13:50:44 +0300

    Delete symnmfmodule.c

--- symnmfmodule.c
+++
@@ -1,9 +0,0 @@
-#ifndef SYMNMF_H
-#define SYMNMF_H
-
-void sym(double** X, double** A, int n, int dim);
-void ddg(double** A, double** D, int n);
-void norm(double** A, double** D, double** W, int n);
-void symnmf(double** W, double **H, int n, int k, int max_iter, double epsilon);
-
-#endif
